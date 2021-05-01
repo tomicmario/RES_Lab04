a33bdd7bbe5a6e9c6c074a676589e377d9bf32a1
Mario Tomic <mario-tomic@hotmail.com>
2021-05-01 16:47:52 +0200

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   dockerFiles/MockMock.jar
    	new file:   readme.md
    	new file:   src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package ch.heig-vd.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
