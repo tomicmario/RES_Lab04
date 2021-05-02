4f9334333bb3e1049c7d197d1f8381939cf5b06d
Mario Tomic <mario-tomic@hotmail.com>
2021-05-02 19:54:32 +0200

    Adaptation du main pour qu'il fasse le boulot demandé
     Changes to be committed:
    	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -7,18 +8,34 @@ import java.util.ArrayList;
 public class Main
 {
     static Client client;
-    static FileReader fileReader = new FileReader();
+    static final int MIN_VICTIMS = 3;
 
-    public static void main( String[] args )
-    {
-        String sender = fileReader.getSender();
-        ArrayList<String> victims = fileReader.getVictims();
-        client = new Client("LOCALHOST",0);
-        ArrayList<Mail> mails = fileReader.getMessage();
-        for (Mail m : mails) {
-            m.setTo(victims);
-            m.setFrom(sender);
-            client.send(m);
+    public static void main( String[] args ) throws IOException {
+
+        ConfigManager conf = new ConfigManager();
+        ArrayList<String> victims = conf.getVictims();
+        client = new Client(conf.getAddress(), conf.getPort());
+        ArrayList<Mail> mails = conf.getMessage();
+        ArrayList<Integer> groups = conf.getGroupsNumbers();
+
+        int currentVictimsIndex = 0;
+        for (int i = 0; i < mails.size(); ++i) {
+            Mail currentMail = mails.get(i);
+            ArrayList<String> currentVictims = new ArrayList<>();
+
+            for(int j = 0; j < groups.get(i); ++j) {
+                currentVictims.add(victims.get(currentVictimsIndex + j));
+            }
+            if(currentVictims.size() < MIN_VICTIMS){
+                System.out.println("Le mail numero " + i + " n'a pas été envoyé, car contient moins de victimes que le nombre minimal : " + MIN_VICTIMS);
+                continue;
+            }
+            currentVictimsIndex += currentVictims.size();
+
+            currentMail.setFrom(currentVictims.get(0));
+            currentVictims.remove(0);
+            currentMail.setTo(currentVictims);
+            client.send(currentMail);
         }
 
     }
